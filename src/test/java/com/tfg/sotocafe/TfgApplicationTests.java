c9fe8b027bafdc4ea50c987d47e07d37de5fa709
Pedro Pablo Carvajal Moreno <p.pablocarvajalmoreno@gmail.com>
2022-02-28 17:09:13 +0100

    Proyect

---
+++ src/test/java/com/tfg/sotocafe/TfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.sotocafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
